6465e8add38e87c5829c71993d6a0e59f460687e
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-10-16 15:28:22 +0200

    Trying watch multi-task

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
         },
         watch: {
             files: ['sass/*'],
-            tasks: ['sass']
+            tasks: ['sass','cssmin']
         },
     });
 
