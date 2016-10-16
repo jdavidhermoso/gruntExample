51094abc37b2c57fe0be9aad5ce14ebdc8f29a23
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-10-16 12:12:49 +0200

    Trying mangle option to prevent var change names

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         uglify: {
+            options: {
+                mangle: false
+            },
             deploy: {
                 files: {
                     'deploy/main.min.js': ['js/testFile1.js']
--- js/testFile1.js
+++ js/testFile1.js
@@ -1,3 +1,4 @@
 (function(){
-    console.log('a');
+    var my_variable = 3;
+    console.log('Playing with mangle option. Variable my_variable value: '+my_variable);
 })();
