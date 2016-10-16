7d1834001913a51e38beffbafd60d0c06501a5cf
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-10-16 15:25:29 +0200

    Watch task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,13 +26,18 @@ module.exports = function (grunt) {
                     'deploy/main.min.css': ['deploy/main.css']
                 }
             }
-        }
+        },
+        watch: {
+            files: ['sass/*'],
+            tasks: ['sass']
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass','watch']);
     grunt.registerTask('prod', ['uglify:prod','sass','cssmin']);
 };
